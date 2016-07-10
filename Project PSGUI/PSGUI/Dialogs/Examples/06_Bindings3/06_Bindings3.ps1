f6c26de5aabe78fc7d4afe896287854b4d089a5e
David das Neves <david_das_neves@gmx.de>
2016-07-10 16:17:08 +0200

    Bugfix Examples

--- Project PSGUI/PSGUI/Dialogs/Examples/06_Bindings3/06_Bindings3.ps1
+++ Project PSGUI/PSGUI/Dialogs/Examples/06_Bindings3/06_Bindings3.ps1
@@ -12,28 +12,34 @@
 #> 
 #region PreFilling
 
-
-$prozesse = Get-Process | Select-Object -First 20
+$06_Bindings3.Add_Loaded(
+	{
+		$prozesse = Get-Process | Select-Object -First 20
  
-#Binding with setup bindinds in xaml file
-$06_Binding3_lvVariables.ItemsSource = $prozesse
-
+		#Binding with setup bindinds in xaml file
+		$06_Bindings3_lv.ItemsSource = $prozesse
+	}
+)
 #endregion
 
 
 #region EventHandler
 
 
-$06_Binding3_btnStop.add_Click(
-    {
+$06_Bindings3_btnStop.add_Click(
+   {
         [System.Object]$sender = $args[0]
         [System.Windows.RoutedEventArgs]$e = $args[1]   
         $prozesse = Get-Process | Select-Object -First 6
-        $06_Binding3_cbbDienste.ItemsSource = $prozesse
-        Stop-Process -Name $06_Binding3_cbbDienste.SelectedValue.Name
+        $06_Bindings3_cbbDienste.ItemsSource = $prozesse
+        Stop-Process -Name $06_Bindings3_cbbDienste.SelectedValue.Name
     }
 )
  
 
 #endregion
+
+
+
+
 
---
+++ Project PSGUI/PSGUI/Dialogs/Examples/07_Bindings_Complete/07_Bindings_Complete.ps1
@@ -0,0 +1,72 @@
+<#	
+    .NOTES
+    ===========================================================================
+        Created on:   	
+        Created by:   	
+        Version:        
+        Project:        
+        Filename:       .ps1
+    ===========================================================================
+    .DESCRIPTION
+        Functions 
+#> 
+#region PreFilling
+
+
+
+#endregion
+
+$prozesse = Get-Process | Select-Object -First 20
+ 
+#Binding by Memberpath
+$07_Bindings_Complete_cbbDienste.DisplayMemberPath = 'Name'
+$07_Bindings_Complete_cbbDienste.ItemsSource = $prozesse
+$07_Bindings_Complete_cbbDienste.SelectedIndex = 0
+ 
+#Binding with setup bindinds in xaml file
+$07_Bindings_Complete_lvVariables.ItemsSource = $prozesse
+ 
+#Binding with PSCustomObjects
+[PSCustomObject]$paramList = @()
+Get-Process |
+Select-Object -First 20 |
+ForEach-Object -Process {
+    $paramList += @([PSCustomObject]@{
+            Set  = if($_.Name.Length -gt 8) 
+            {
+                'bold'
+            }
+            else 
+            {
+                'normal'
+            }
+            Name = $_.Name
+        }      
+    )
+} 
+  
+$07_Bindings_Complete_lvEvents.ItemsSource = $paramList
+
+#region EventHandler
+
+$07_Bindings_Complete_cbbDienste.add_SourceUpdated(
+    {
+        [System.Object]$sender = $args[0]
+        [System.Windows.RoutedEventArgs]$e = $args[1]   
+        #$07_Bindings_Complete_cbbDienste.ItemsSource = $prozesse
+    }
+)
+ 
+ 
+$07_Bindings_Complete_btnStop.add_Click(
+    {
+        [System.Object]$sender = $args[0]
+        [System.Windows.RoutedEventArgs]$e = $args[1]   
+        $prozesse = Get-Process | Select-Object -First 6
+        $07_Bindings_Complete_cbbDienste.ItemsSource = $prozesse
+        Stop-Process -Name $07_Bindings_Complete_cbbDienste.SelectedValue.Name
+    }
+)
+
+
+#endregion
